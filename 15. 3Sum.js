721303b1323feb261e8f2c817390a3183991942f
Arslan Ali <arsxlanali@gmail.com>
2022-08-26 22:39:50 +0500

    Update 15. 3Sum.js

--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -1,21 +1,48 @@
+
+function isEqual(a, b)
+{
+    return a.join() == b.join();
+}
+function compare(result, array1)
+{
+  for(let array of result)
+  {
+    if(isEqual(array,array1))
+    {
+      return true;
+    }a
+  }
+  return false;
+}
+
 var threeSum = function(nums) {
     let len = nums.length-2;
-  	var array = [];
+  	nums.sort();
+  	var result =[];
+  	if(nums.every((e)=>e==0))
+      return [[0,0,0]];
+  	let i = 0;
   	for(let j=0; j<nums.length-2; j++)
     {
       for(let k=0; k<len;k++)
       {
-        if(nums[0]+nums[j+1]+nums[j+k+2]==0)
+        if(nums[i]+nums[j+1]+nums[j+k+2]==0)
         {
-          array.push()
+          //console.log(i,j+1, k+2);
           
+          let array = [];
+          array.push(nums[i]);
+          array.push(nums[j+1]);
+          array.push(nums[j+k+2]);
+          if(!compare(result,array))
+          {
+            result.push(array);
+          }
         }
         
       }
+      //i++;
       len--;
     }
+  	return result;
 };
-
-var array = [-1,0,1,2,-1,4];
-console.log(threeSum(array));
-// expected output: 1
