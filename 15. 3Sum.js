d3516c390a17c622a4df1fa7a16ff7de4a2de53a
Arslan Ali <arsxlanali@gmail.com>
2022-08-27 23:35:29 +0500

    Update 15. 3Sum.js

--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -1,48 +1,31 @@
-
-function isEqual(a, b)
-{
-    return a.join() == b.join();
-}
-function compare(result, array1)
-{
-  for(let array of result)
-  {
-    if(isEqual(array,array1))
-    {
-      return true;
-    }a
-  }
-  return false;
-}
-
 var threeSum = function(nums) {
-    let len = nums.length-2;
-  	nums.sort();
-  	var result =[];
-  	if(nums.every((e)=>e==0))
-      return [[0,0,0]];
-  	let i = 0;
-  	for(let j=0; j<nums.length-2; j++)
-    {
-      for(let k=0; k<len;k++)
-      {
-        if(nums[i]+nums[j+1]+nums[j+k+2]==0)
-        {
-          //console.log(i,j+1, k+2);
-          
-          let array = [];
-          array.push(nums[i]);
-          array.push(nums[j+1]);
-          array.push(nums[j+k+2]);
-          if(!compare(result,array))
-          {
-            result.push(array);
-          }
+    let res = [];
+    nums.sort((a,b) => a - b);
+    
+    nums.forEach((el, i) => {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            return;
         }
         
-      }
-      //i++;
-      len--;
-    }
-  	return result;
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while (left < right) {
+            let sum = el + nums[left] + nums[right];
+            
+            if (sum > 0) {
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                res.push([el, nums[left], nums[right]]);
+                left++;
+                while (left < right && nums[left] === nums[left - 1]) {
+                    left++;
+                }
+            }
+        }
+    })
+    
+    return res;
 };
